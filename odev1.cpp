a84cb8c278904d8240fd8fc7a600d964f96bc2d6
Ahmet Yunus Çavuş <ahmetyunus30q@gmail.com>
2023-11-13 13:06:09 +0300

    Update odev1.cpp

--- odev1.cpp
+++ odev1.cpp
@@ -1,5 +1,7 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -55,6 +57,192 @@ public:
         }
     }
 
+    int usAlma(int taban, int us)
+    {
+        int sonuc = 1;
+        if (us > 0)
+        {
+            for (int i = 0; i < us; i++)
+            {
+                sonuc *= taban;
+            }
+            return sonuc;
+        }
+        else if (us == 0)
+            return 1;
+        else
+            return 0;     
+    }
+
+    int sayiDegeri()
+    {
+        Basamak* basamak = ilk;
+        int basamaksayisi = 0;
+        int toplam = 0;
+
+        while (basamak != 0)
+        {
+            basamaksayisi++;
+            basamak = basamak->sonraki;
+        }
+        basamak = ilk;
+
+        while (basamak != 0)
+        {
+            toplam += basamak->veri * usAlma(10, basamaksayisi - 1);
+            basamak = basamak->sonraki;
+        }
+        return toplam;
+    }
+
+
+    void tekleriBasaAl()
+    {
+        Basamak* sira = ilk;
+        Basamak* onceki = 0;
+        Basamak* suanki = 0;
+        Basamak* sonraki = 0;
+
+        int basamaksayisi = 0;
+        suanki = ilk;
+        while (suanki != 0)
+        {
+            basamaksayisi++;
+            suanki = suanki->sonraki;
+        }
+        suanki = 0;
+
+
+        if (basamaksayisi >= 3)
+        {
+
+            Basamak* onceki = ilk;
+            Basamak* suanki = onceki->sonraki;
+            Basamak* sonraki = suanki->sonraki;
+            bool tek = true;
+
+            if (ilk->veri % 2 == 1)
+            {
+                do
+                {
+                    if (suanki->veri % 2 == 1)
+                    {
+                        if (sira->sonraki == suanki)
+                        {
+                            sira = suanki;
+                            onceki = suanki;
+                            suanki = sonraki;
+                            sonraki = sonraki->sonraki;
+                        }
+                        else
+                        {
+                            onceki->sonraki = sonraki;
+                            suanki->sonraki = sira->sonraki;
+                            sira->sonraki = suanki;
+                            sira = suanki;
+                            suanki = sonraki;
+                            sonraki = sonraki->sonraki;
+                        }
+                    }
+                    else
+                    {
+                        onceki = suanki;
+                        suanki = sonraki;
+                        sonraki = sonraki->sonraki;
+                    }
+                } while (suanki->sonraki != 0);
+            }
+            else
+            {
+                do
+                {
+                    if (suanki->veri % 2 == 1 && tek)
+                    {
+                        onceki->sonraki = sonraki;
+                        suanki->sonraki = sira;
+                        sira = suanki;
+                        ilk = suanki;
+                        suanki = sonraki;
+                        sonraki = sonraki->sonraki;
+                        tek = false;
+                    }
+                    else if (suanki->veri % 2 == 1)
+                    {
+                        onceki->sonraki = sonraki;
+                        suanki->sonraki = sira->sonraki;
+                        sira->sonraki = suanki;
+                        sira = suanki;
+                        suanki = sonraki;
+                        sonraki = sonraki->sonraki;
+                    }
+                    else
+                    {
+                        onceki = suanki;
+                        suanki = sonraki;
+                        sonraki = sonraki->sonraki;
+                    }
+                } while (suanki->sonraki != 0);
+            }
+
+            if (suanki->veri % 2 == 1)
+            {
+                suanki->sonraki = sira->sonraki;
+                sira->sonraki = suanki;
+                onceki->sonraki = 0;
+            }
+
+
+        }
+    }
+
+
+    void sayiyiTersCevir()
+    {
+        Basamak* onceki = 0;
+        Basamak* suanki = 0;
+        Basamak* sonraki = 0;
+
+        int basamaksayisi = 0;
+        suanki = ilk;
+        while (suanki != 0)
+        {
+            basamaksayisi++;
+            suanki = suanki->sonraki;
+        }
+        suanki = 0;
+
+        if (basamaksayisi == 2)
+        {
+            suanki = ilk;
+            sonraki = ilk->sonraki;
+            sonraki->sonraki = suanki;
+            suanki->sonraki = 0;
+            ilk = sonraki;
+        }
+
+        else if (basamaksayisi >= 3)
+        {
+            onceki = ilk;
+            suanki = ilk->sonraki;
+            sonraki = suanki->sonraki;
+
+            do
+            {
+                suanki->sonraki = onceki;
+                onceki = suanki;
+                suanki = sonraki;
+                sonraki = sonraki->sonraki;
+
+            } while (sonraki != 0);
+
+            suanki->sonraki = onceki;
+            ilk->sonraki = 0;
+            ilk = suanki;
+        }
+    }
+       
+
+
     void yazdir()
     {
         Basamak* gecici = ilk;
@@ -64,7 +252,6 @@ public:
         while (gecici != 0)
         {
             basamaksayisi++;
-
             gecici = gecici->sonraki;
         }
 
@@ -171,6 +358,73 @@ public:
         ilk = 0;
     }
 
+    void sayiyiTersCevir()
+    {
+        Dugum* geciciDugum = ilk;
+
+        while (geciciDugum != 0)
+        {
+            geciciDugum->sayi->sayiyiTersCevir();
+
+            geciciDugum = geciciDugum->sonraki;
+        }
+
+    }
+
+    void enbuyukSil()
+    {
+        Dugum* onceki = 0;
+        Dugum* sonraki = 0;
+        Dugum* suanki = ilk;
+        Dugum* sil = 0;
+        int buyukSayi = ilk->sayi->sayiDegeri();
+        cout << "aaaaaaaaaaaaaaaaaaaaaa : " << buyukSayi << endl;
+
+        int basamaksayisi = 0;
+        while (suanki != 0)
+        {
+            basamaksayisi++;
+            suanki = suanki->sonraki;
+        }
+        suanki = ilk;
+
+        if (basamaksayisi == 1)
+        {
+
+        }
+        else if (basamaksayisi == 2)
+        {
+
+        }
+        else if (basamaksayisi >= 3)
+        {
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+    void tekleriBasaAl()
+    {
+        Dugum* geciciDugum = ilk;
+
+        while (geciciDugum != 0)
+        {
+            geciciDugum->sayi->tekleriBasaAl();
+
+            geciciDugum = geciciDugum->sonraki;
+        }
+
+    }
 
     void sonaEkle(int veri)
     {
@@ -233,18 +487,82 @@ public:
 
 int main()
 {
-
+    /* Verileri dosyada okuma kismi*/
     SayilarListesi* liste = new SayilarListesi();
 
-    liste->sonaEkle(1);
-    liste->sonaEkle(12);
+    string dosyaAdi = "Sayilar.txt";
 
-    liste->yazdir();
+    ifstream dosya(dosyaAdi);
+
+    if (!dosya.is_open())
+    {
+        cerr << "Sayilar.txt dosyasi acilamadi.\n";
+        return 1;
+    }
+
+    string satir;
+    getline(dosya, satir);
+
+    istringstream ss(satir);
+
+    int sayi;
+
+    while (ss >> sayi)
+    {
+        liste->sonaEkle(sayi);
+    }
+
+    /* Verileri dosyada okuma kismi*/
+
+
+    /* Menu basma asamasi */
+    int secim = 0;
+    while (secim != 4)
+    {
+        liste->yazdir();
+
+        cout << "1. Tek basamaklari Basa Al\n";
+        cout << "2. Basamaklari Tersle\n";
+        cout << "3. En Buyuk Sayiyi Cikar\n";
+        cout << "4. Cikis\n\n";
+
+        cout << "Yapmak istediginiz islemi tuslayiniz:  ";
+        cin >> secim;
+        cout << "\n\n";
+
+        while (secim < 1 && secim > 4)
+        {
+            cout << "Olmayan bir islemi sectiniz lutfen 1,2,3,4 tuslarindan birini tuslayiniz.\n";
+            cin >> secim;
+        }
+        
+        if (secim == 1)
+        {
+            liste->tekleriBasaAl();
+        }
+        else if (secim == 2)
+        {
+            liste->sayiyiTersCevir();
+        }
+        else if (secim == 3)
+        {
+            liste->enbuyukSil();
+        }
+        else if (secim == 4)
+        {
+            cout << "Cikis yapiliyor.";
+        }
+        else
+        {
+            cout << "BUG";
+        }
+    }
+    /* Menu basma asamasi */
 
 
 
 
-    cin.get();
+    cin >> secim;
 
     return 0;
 }
